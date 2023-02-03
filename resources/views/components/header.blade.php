4cf2cd279e7f1fb53c37566625c6f7978b603bb1
mintybubblegum <anamaria.tz@hotmail.com>
2023-02-03 13:30:39 +0100

    updated header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,7 @@
-<div>
-    <h3>Welcome to the first ever school for outcasts with normie subjects</h3>
+<div class="headerBox">
+    <img src="img/nevermoreLogo.png" class="logo" alt="Nevermore Academy Logo">
+    <a class="navbar-brand headerTitle" href="{{ url('/') }}">
+        Nevermore Academy
+    </a>
+    <p class="welcomeTxt">Welcome to the first ever school for outcasts with normie subjects</p>
 </div>
