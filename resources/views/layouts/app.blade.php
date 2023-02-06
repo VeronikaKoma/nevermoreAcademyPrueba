28629f954b4fe650a6c29b7b8cfc7e02bf98b394
VeronikaKoma <116543383+VeronikaKoma@users.noreply.github.com>
2023-02-06 04:31:00 +0100

    add login block

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,18 +16,35 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body class="bodyApp">
+<body>
+
+<div class="loginBox">
+        <div class="email">
+            <label for="email"><p style="color:#EAA41C" word-spacing: 10px>Outcast Email</b></label>
+                    <input type="email" placeholder="Enter Email" name="email" required>
+        </div>
+    
+        <div class="password">
+            <label for="psw"><b style="color:#EAA41C">Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="psw" required>
+        </div>
+
+    <div class="btn-continue">
+            <input type="submit" value="CONTINUE">
+        </div>
+</div>
+
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
 
@@ -56,8 +73,8 @@
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
