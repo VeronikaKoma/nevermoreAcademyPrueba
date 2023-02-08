c30182a93424bdc0ceb0dd9dd1454962bb9979fd
AlesiaCoder <alesia.nekolethal@gmail.com>
2023-02-08 13:25:40 +0100

    footer optimized

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,19 +1,11 @@
 <footer class= "footerBox">
     <div class="socialMedia">
-        <a class="navbar-brand" href="{{ url('https://www.instagram.com/nevermoreacademy/') }}" target="_blank"
->
+        <a class="navbar-brand" href="{{ url('https://www.instagram.com/nevermoreacademy/') }}" target="_blank">
             <img class="socialMedia" src="img/instagramBtn.png" alt="nevermoreInstagram">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
         </a>
 
-        <a class="navbar-brand" href="{{ url('https://www.youtube.com/watch?v=SoTmSuThMKc') }}" target="_blank"
->
+        <a class="navbar-brand" href="{{ url('https://www.youtube.com/watch?v=SoTmSuThMKc') }}" target="_blank">
             <img class="socialMedia" src="img/youtubeBtn.png" alt="nevermoreYoutube">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
         </a>
     </div>
 
