a901af46c7160132baada9a1d519f11ed5aa9ba4
Nataliaplm <nataliapalomof@gmail.com>
2023-02-08 16:30:49 +0100

    changes on create

--- tests/Feature/CRUDGradeTest.php
+++ tests/Feature/CRUDGradeTest.php
@@ -21,4 +21,37 @@ class CRUDGradeTest extends TestCase
         
         $this->assertCount(1, Grade::all());
     }
+
+    public function test_aGradeCanBeCreated(){
+        
+            $this->withExceptionHandling();
+
+            $userTeacher = Grade::factory()->create(['isTeacher'=>true]);
+            $this->actingAs($userTeacher);
+            $response = $this->post(route('storeUser'),
+            [
+                'name' => 'name',
+                'surname' => 'surname',
+                'email' => 'email',
+                'password' => 'password',
+                'img' => 'img',
+                'currentTerm' => 'currentTerm'
+            ]);
+            $this->assertCount(2, User::all());
+
+            $user = Grade::factory()->create(['isTeacher'=>false]);
+            $this->actingAs($user);
+            $response = $this->post(route('storeUser'),
+            [
+                'name' => 'name',
+                'surname' => 'surname',
+                'email' => 'email',     
+                'password' => 'password',
+                'img' => 'img',
+                'currentTerm' => 'currentTerm'
+            ]);
+
+            $this->assertCount(3, User::all());
+        
+    }
 }
