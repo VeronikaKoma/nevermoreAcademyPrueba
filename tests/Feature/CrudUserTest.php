821da6d1f3712a63ddf729d0223e7505341d0192
AlesiaCoder <alesia.nekolethal@gmail.com>
2023-02-10 10:19:39 +0100

    merge stable headerFooter

--- tests/Feature/CrudUserTest.php
+++ tests/Feature/CrudUserTest.php
@@ -1,12 +1,9 @@
 <?php
-
 namespace Tests\Feature;
-
 use Tests\TestCase;
 use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-
 class CRUDUserTest extends TestCase
 {
     /**
@@ -14,13 +11,10 @@ class CRUDUserTest extends TestCase
      *
      * @return void
      */
-
     use RefreshDatabase;
-    
     /* public function test_example()
     {
         $response = $this->get('/');
-
         $response->assertStatus(200);
     } */
 
@@ -32,16 +26,33 @@ class CRUDUserTest extends TestCase
         $response->assertSee($user->name);
         $response->assertStatus(200)
         ->assertViewIs('home');
-    }  
+    }
 
     public function test_aUserCanBeDeleted(){
         $this->withExceptionHandling();
-
         $user = User::factory()->create();
         $this->assertCount(1, User::all());
-
         $response = $this->delete(route("deleteUser", $user->id));
-
         $this->assertCount(0, User::all());
+    }
+
+    public function test_anUserCanBeUpdated(){
+        $this->withExceptionHandling();
+        $user = User::factory()->create();
+        $this->assertCount(1, User::all());
+    }
+    
+    public function test_aUserCanBeCreated(){
+        $this->withExceptionHandling();
+        $response = $this->post(route('storeUser'),
+        [
+            'name' => 'name',
+            'surname' => 'surname',
+            'email' => 'email',
+            'password' => 'password',
+            'img' => 'img',
+            'currentTerm' => 'currentTerm'
+        ]);
+        $this->assertCount(1, User::all());
     }
 }
